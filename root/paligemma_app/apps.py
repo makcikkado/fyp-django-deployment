9b886db298348ca4d6aef47a759b855bdcd5727e
makcikkado <151117832+makcikkado@users.noreply.github.com>
2025-06-17 19:34:41 +0800

    Update apps.py
    added signals

--- root/paligemma_app/apps.py
+++ root/paligemma_app/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'paligemma_app'
+
+    def ready(self):
+        import paligemma_app.signals
